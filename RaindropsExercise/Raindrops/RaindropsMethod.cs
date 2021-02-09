df43c3413ff53baef7b6a7f4e4443b3193e76cef
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2021-02-09 11:27:51 +0000

    Setup project, added method for determining factors of input

---
+++ RaindropsExercise/Raindrops/RaindropsMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Raindrops
+{
+    public static class RaindropsMethod
+    {
+        public static string FactorFinder(int n)
+        {
+            string output = "";
+            output += (n % 3 == 0) ? "Pling" : "";
+            output += (n % 5 == 0) ? "Plang" : "";
+            output += (n % 7 == 0) ? "Plong" : "";
+            output += (output == "") ? n.ToString() : "";
+
+            return output;
+        }
+    }
+}
