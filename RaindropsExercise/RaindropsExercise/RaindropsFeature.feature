e6b9e8b0ad34c2d22a137701dacccc21d61f7d99
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2021-02-09 11:34:26 +0000

    Added feature file for Specflow tests

---
+++ RaindropsExercise/RaindropsExercise/RaindropsFeature.feature
@@ -0,0 +1,16 @@
+﻿Feature: RaindropsFeature
+	Utility for giving a certain concatenated output given if the input has factors of 3, 5 and/or 7
+
+@mytag
+Scenario: A number has a single factor out of 3, 5 and 7
+	Given I input a number <input1> which has a single factor
+	When the factor finder method is called
+	Then the result is the appropriate string <result>
+	Examples:
+	| input1 | result  |
+	| 3      | "Pling" |
+	| 6      | "Pling" |
+	| 5      | "Plang" |
+	| 10     | "Plang" |
+	| 7      | "Plong" |
+	| 14     | "Plong" |
---
+++ RaindropsExercise/RaindropsExercise/RaindropsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace RaindropsExercise
+{
+    public class RaindropsTests
+    {
+        [Given(@"I input a number (.*) which has a single factor")]
+        public void GivenIInputANumberWhichHasASingleFactor(int n)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the factor finder method is called")]
+        public void WhenTheFactorFinderMethodIsCalled()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result is the appropriate string (.*)")]
+        public void ThenTheResultIsTheAppropriateString(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
